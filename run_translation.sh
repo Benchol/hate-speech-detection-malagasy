71ff34dfc2b4dd465a26f7e91d5a12714926878b
Riana Andria <riana@mg.corellia.be>
2025-05-23 15:55:45 +0300

    add racism dataset on script bash

--- run_translation.sh
+++ run_translation.sh
@@ -24,6 +24,13 @@ python script_translation.py \
     --batch_size 32 \
     --file_id "1171e_GbrxhU8k9kDUpTp-8LolqjioDCo" &
 
+# --- Traduction 3: DataSet Tweet racism(nbre_rows = 13.471)
+python script_translation.py \
+    --start_index 0 \
+    --end_index 13470 \
+    --batch_size 32 \
+    --file_id "1--V-kyXYA88hwW5dqwbgK8CbyUxBVrkX" &
+
 # --- Attente de la fin de toutes les tâches ---
 wait
 echo "Toutes les tâches de traduction sont terminées."
