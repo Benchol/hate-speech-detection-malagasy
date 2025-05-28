d53d0b077678011421be5936e9b460e9d6c98183
Riana Andria <riana@mg.corellia.be>
2025-05-28 22:55:32 +0300

    update data list

--- run_translation.sh
+++ run_translation.sh
@@ -10,33 +10,40 @@ python script_translation.py \
     --batch_size 32 \
     --file_id "1phccMgk_1KxNsCkcigjhkHM44w7Qy_Aa" &
 
-# --- Traduction 2: DataSet HateXplainDataset(nbre_rows = 19.229)
+# --- Traduction 2: DataSet HateXplainDataset(nbre_rows = 19.229) OK
 python script_translation.py \
     --start_index 0 \
     --end_index 19228 \
     --batch_size 32 \
     --file_id "1NIavkN5INEV68cOpgfH6iUXrc7aAvzTa" &
 
-# --- Traduction 3: DataSet Kurrek Slur Corpus(nbre_rows = 40.003)
+# --- Traduction 3: DataSet Kurrek Slur Corpus(nbre_rows = 40.003) OK
 python script_translation.py \
     --start_index 0 \
     --end_index 40002 \
     --batch_size 32 \
     --file_id "1171e_GbrxhU8k9kDUpTp-8LolqjioDCo" &
 
-# --- Traduction 4: DataSet OLID(nbre_rows = 13.240)
+# --- Traduction 4: DataSet OLID(nbre_rows = 13.240) OK
 python script_translation.py \
     --start_index 0 \
     --end_index 13239 \
     --batch_size 32 \
     --file_id "1-7rs2hYnzSZ76AxTFOKnend97QHh9qUS" &
 
-# --- Traduction 5: DataSet DynamicallyGeneratedHateDataset (nbre_rows = 40.622)
+# --- Traduction 5: DataSet DynamicallyGeneratedHateDataset (nbre_rows = 40.622) OK
 python script_translation.py \
     --start_index 0 \
     --end_index 40621 \
     --batch_size 32 \
     --file_id "1-7ziVqFY-4zUwHoZ_NIkxFGYmDEevr-p" &
+
+# --- Traduction 6: DataSet CAD (nbre_rows = 13.585) 
+python script_translation.py \
+    --start_index 0 \
+    --end_index 13584 \
+    --batch_size 32 \
+    --file_id "1owVVMMkES0ye4fLJmThKF3ZvRB4YjoJ2" &
 
 # --- Attente de la fin de toutes les tâches ---
 wait
